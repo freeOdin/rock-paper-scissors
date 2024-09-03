de12da040843a8093d2c6b13650d9bc7b927c2a2
freeOdin <159131665+freeOdin@users.noreply.github.com>
2024-09-03 21:27:57 +0100

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Testing ...");
+function getComputerChoice () {
+  const num = Math.floor(Math.random() * 3) + 1;
+
+  if (num == 1) {
+    return "rock";
+  } else if (num == 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice())
